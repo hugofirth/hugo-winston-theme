5aa47bbeb31f2b10da5fd5edaec5fd8f22e96044
Hugo Firth <me@hugofirth.com>
2022-12-19 16:09:01 +0000

    Dark mode continues

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,11 +2,18 @@ var body = document.querySelector('body');
 var menuTrigger = document.querySelector('#toggle-menu-main-mobile');
 var menuContainer = document.querySelector('#menu-main-mobile');
 var hamburgerIcon = document.querySelector('.hamburger');
+var darkmodeTrigger = document.querySelector('#toggle-dark-mode');
 
 if (menuTrigger !== null) {
   menuTrigger.addEventListener('click', function(e) {
     menuContainer.classList.toggle('open');
     hamburgerIcon.classList.toggle('is-active');
     body.classList.toggle('lock-scroll');
+  });
+}
+
+if (darkmodeTrigger !== null) {
+  darkmodeTrigger.addEventListener('click', function(e) {
+    body.classList.toggle('dark-mode');	  
   });
 }
