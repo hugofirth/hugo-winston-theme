d1f8e33d433224b4816b5d8fccbacc3f22da57f8
Hugo Firth <me@hugofirth.com>
2025-03-23 23:34:35 +0000

    Fixed some darkmode foibles including adding custom icon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@ var menuTrigger = document.querySelector('#toggle-menu-main-mobile');
 var menuContainer = document.querySelector('#menu-main-mobile');
 var hamburgerIcon = document.querySelector('.hamburger');
 var darkmodeTrigger = document.querySelector('#toggle-dark-mode');
+var darkmodeIcon = document.querySelector('#dark-mode-icon');
 
 if (menuTrigger !== null) {
   menuTrigger.addEventListener('click', function(e) {
@@ -14,6 +15,14 @@ if (menuTrigger !== null) {
 
 if (darkmodeTrigger !== null) {
   darkmodeTrigger.addEventListener('click', function(e) {
-    body.classList.toggle('dark-mode');	  
+    const added = body.classList.toggle('dark-mode');
+    if (added && darkmodeIcon !== null) {
+      darkmodeIcon.classList.remove('bi-sun');
+      darkmodeIcon.classList.add('bi-moon');
+    } else if(darkmodeIcon !== null) {
+      darkmodeIcon.classList.add('bi-sun');
+      darkmodeIcon.classList.remove('bi-moon');
+    }
   });
 }
+
