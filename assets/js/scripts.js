ef1a281360c0bab42a71d940d7d24009bcf97b88
Hugo Firth <me@hugofirth.com>
2025-03-24 22:11:30 +0000

    Fixed dark mode toggle

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,21 @@
-var body = document.querySelector('body');
-var menuTrigger = document.querySelector('#toggle-menu-main-mobile');
-var menuContainer = document.querySelector('#menu-main-mobile');
-var hamburgerIcon = document.querySelector('.hamburger');
-var darkmodeTrigger = document.querySelector('#toggle-dark-mode');
-var darkmodeIcon = document.querySelector('#dark-mode-icon');
+const body = document.querySelector('body');
+const menuTrigger = document.querySelector('#toggle-menu-main-mobile');
+const menuContainer = document.querySelector('#menu-main-mobile');
+const hamburgerIcon = document.querySelector('.hamburger');
+const darkmodeTrigger = document.querySelector('#toggle-dark-mode');
+const darkmodeIcon = document.querySelector('#dark-mode-icon');
+const isDarkMode = localStorage.getItem('darkMode') === 'true' || 
+	(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+if (isDarkMode) {
+  body.classList.add('dark-mode');
+  darkmodeIcon.classList.remove('bi-moon');
+  darkmodeIcon.classList.add('bi-sun');
+} else {
+  body.classList.remove('dark-mode');
+  darkmodeIcon.classList.remove('bi-sun');
+  darkmodeIcon.classList.add('bi-moon');
+}
 
 if (menuTrigger !== null) {
   menuTrigger.addEventListener('click', function(e) {
@@ -17,11 +29,11 @@ if (darkmodeTrigger !== null) {
   darkmodeTrigger.addEventListener('click', function(e) {
     const added = body.classList.toggle('dark-mode');
     if (added && darkmodeIcon !== null) {
-      darkmodeIcon.classList.remove('bi-sun');
-      darkmodeIcon.classList.add('bi-moon');
-    } else if(darkmodeIcon !== null) {
-      darkmodeIcon.classList.add('bi-sun');
       darkmodeIcon.classList.remove('bi-moon');
+      darkmodeIcon.classList.add('bi-sun');
+    } else if(darkmodeIcon !== null) {
+      darkmodeIcon.classList.add('bi-moon');
+      darkmodeIcon.classList.remove('bi-sun');
     }
   });
 }
